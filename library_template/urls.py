413305976659d796f10b26423f6456ebf1eac9bc
keroo <kerooadel5@gmail.com>
2023-06-10 04:41:16 +0300

    edit step2 in ai article writer

--- library_template/urls.py
+++ library_template/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('ai-article-writer/keywords/', views.AiArticleWriterView.as_view() , name='ai article writer'),
+    path('ai-article-writer/ideas/', views.IdeasGenratorView.as_view() , name='get ideas'),
+    path('ai-article-writer/outlines/', views.OutlinesGenratorView.as_view() , name='get outlines'),
+    path('ai-article-writer/article/', views.ArticleGenratorView.as_view() , name='get article'),
+    
+    
     path('email-tools/', views.EmailToolsView.as_view() , name='email tools'),
     path('SocialMedia-tools/', views.SocialMediaToolsView.as_view() , name='social media tools'),
     
--- library_template/views.py
+++ library_template/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 
+############################### 
+# AiArticleWriter template
+############################### 
 class AiArticleWriterView(LoginRequiredMixin,ListView):
     template_name = 'ai_article_writer/aIArticleWriter.html'
     
@@ -19,18 +22,44 @@ class AiArticleWriterView(LoginRequiredMixin,ListView):
         if message:
             return render(request, 'ai_article_writer/keywords.html', 
                           {'keywordslist': keyword_list, 
-                           'message': message
-                                            })
+                           'message': message})
 
         return render(request, self.template_name)
-        
 
+class IdeasGenratorView(LoginRequiredMixin,ListView):
+    template_name = 'ai_article_writer/aIArticleWriter-step-2.html'
+    
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class OutlinesGenratorView(LoginRequiredMixin,ListView):
+    template_name = 'ai_article_writer/aIArticleWriter-step-3.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class ArticleGenratorView(LoginRequiredMixin,ListView):
+    template_name = 'ai_article_writer/aIArticleWriter-step-4.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+
+
+
+############################### 
+# Email Tools template
+############################### 
 class EmailToolsView(LoginRequiredMixin,ListView):
     template_name = 'email_tools/emailTool.html'
     
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
+############################### 
+# Social Media Tools template
+############################### 
 class SocialMediaToolsView(LoginRequiredMixin,ListView):
     template_name = 'social_media_tools/socialMediaTool.html'
     
